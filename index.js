eb86a0a96fddd220e7a96297cddde4d2ff83025a
Megan Johnson <meganjohnson@mac.lan>
2025-01-22 22:48:47 -0500

    update index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// Your code here
+function greet() {
+    let name = prompt("What is your name?");
+    if (name === null || name === "") {
+        alert("Oops! You ddn't enter your name.");
+    } else (name.toLowerCase() === name );
+        alert("Hello, " + name + "!");
+
+    let ageInput = prompt("How old are you?");
+    
+    let age = parseInt(ageInput);
+    if (isNaN(age)) {
+        alert("That's not a valid age.");
+    } else {
+        alert("You are " + age + " years old.");
+    }
+
+    let hasHadBirthday = confirm("Have you had your birthday this year?");
+
+    let birthYear
+        if (hasHadBirthday === true) {
+            birthYear = new Date().getFullYear()  - age;
+            alert("You were born in " + birthYear + ".");
+        } 
+        else [hasHadBirthday === false]; {
+            birthYear = new Date().getFullYear()  - age - 1;
+            alert("You were born in " + birthYear + ".");
+        }
+}
